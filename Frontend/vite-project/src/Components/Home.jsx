cd8742019210ae6b49508a6eb3b71014b8686132
SHUBHANK MAHESHWARI <maheshwarishubhank2002@gmail.com>
2025-08-14 19:44:02 +0530

    added tailwind classes

--- Frontend/vite-project/src/Components/Home.jsx
+++ Frontend/vite-project/src/Components/Home.jsx
@@ -20,42 +20,42 @@ const Home = () => {
   return (
     <div className="flex flex-col gap-25 p-14 items-center">
       <h1 className="text-4xl font-bold">Football Stats</h1>
-      <div className="">
-        <div className="grid grid-cols-3 ">
+      <div className="flex flex-col gap-5">
+        <div className="grid grid-cols-3 gap-5">
           <h1 className="text-center">Date</h1>
           <h1 className="col-span-2 text-center">{stats.date}</h1>
         </div>
-        <div className="grid grid-cols-3 ">
+        <div className="grid grid-cols-3 gap-5">
           <h1 className="text-center"></h1>
           <h1 className="text-center">Team A</h1>
           <h1 className="text-center">Team B</h1>
         </div>
-        <div className="grid grid-cols-3 ">
+        <div className="grid grid-cols-3 gap-5">
           <h1 className="text-center">Goals</h1>
           <h1 className="text-center">{stats.stats.teamA.goals}</h1>
           <h1 className="text-center">{stats.stats.teamB.goals}</h1>
         </div>
-        <div className="grid grid-cols-3 ">
+        <div className="grid grid-cols-3 gap-5">
           <h1 className="text-center">Possession</h1>
           <h1 className="text-center">{stats.stats.teamA.possession}</h1>
           <h1 className="text-center">{stats.stats.teamB.possession}</h1>
         </div>
-        <div className="grid grid-cols-3 ">
+        <div className="grid grid-cols-3 gap-5 ">
           <h1 className="text-center">Total passes</h1>
           <h1 className="text-center">{stats.stats.teamA.passes}</h1>
           <h1 className="text-center">{stats.stats.teamB.passes}</h1>
         </div>
-        <div className="grid grid-cols-3 ">
+        <div className="grid grid-cols-3 gap-5">
           <h1 className="text-center">Shots</h1>
           <h1 className="text-center">{stats.stats.teamA.shots}</h1>
           <h1 className="text-center">{stats.stats.teamB.shots}</h1>
         </div>
-        <div className="grid grid-cols-3 ">
+        <div className="grid grid-cols-3 gap-5">
           <h1 className="text-center">Shots on Target</h1>
           <h1 className="text-center">{stats.stats.teamA.shotsOnTarget}</h1>
           <h1 className="text-center">{stats.stats.teamB.shotsOnTarget}</h1>
         </div>
-        <div className="grid grid-cols-3 ">
+        <div className="grid grid-cols-3 gap-5">
           <h1 className="text-center">Corners</h1>
           <h1 className="text-center">{stats.stats.teamA.corners}</h1>
           <h1 className="text-center">{stats.stats.teamB.corners}</h1>
